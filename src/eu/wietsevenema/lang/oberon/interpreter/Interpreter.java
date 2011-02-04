8da7d95d63cb48ee8f9b245a1c8a9d25f6ec7ce5
Wietse Venema <wietsevenema@gmail.com>
2011-02-04 21:43:03 +0100

    dat was het voorlopig wel even

--- src/eu/wietsevenema/lang/oberon/interpreter/Interpreter.java
+++ src/eu/wietsevenema/lang/oberon/interpreter/Interpreter.java
@@ -1,80 +1,76 @@
 package eu.wietsevenema.lang.oberon.interpreter;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
-import java.io.OutputStreamWriter;
 import java.io.Reader;
 
 import xtc.parser.ParseError;
 import xtc.parser.Result;
 import xtc.parser.SemanticValue;
 import xtc.tree.Node;
-import xtc.tree.Printer;
+import eu.wietsevenema.lang.oberon.ast.declarations.Module;
+import eu.wietsevenema.lang.oberon.exceptions.InvalidInputException;
 import eu.wietsevenema.lang.oberon.parser.Oberon;
 
 public class Interpreter {
 
-	/**
-	 * @param args
-	 * 
-	 * FIXME foutmeldingen met locatie erbij. 
-	 * 
-	 */
 	public static void main(String[] args) {
 		if ((null == args) || (0 == args.length)) {
-			System.err.println("Usage: <file-name>+");
+			System.err.println("Usage: <file-name> <input>+");
 
 		} else {
-			for (int i = 0; i < args.length; i++) {
-				System.err.println("Processing " + args[i] + " ...");
+			String filename = args[0];
+			String newLine = System.getProperty("line.separator");
+			String input = "";
+			for (int i = 1; i < args.length; i++) {
+				input += args[i] + newLine;
+			}
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+			Reader in = null;
+			try {
+				in = new BufferedReader(new FileReader(filename));
+				Oberon p = new Oberon(in, filename,
+						(int) new File(filename).length());
+				Result r = p.pProgram(0);
 
-				Reader in = null;
-				try {
-					in = new BufferedReader(new FileReader(args[i]));
-					Oberon p = new Oberon(in, args[i],
-							(int) new File(args[i]).length());
-					Result r = p.pProgram(0);
+				if (r.hasValue()) {
+					SemanticValue v = (SemanticValue) r;
 
-					if (r.hasValue()) {
-						SemanticValue v = (SemanticValue) r;
-
-						if (v.value instanceof Node) {
-							//FIXME
-							Node result;
-							Printer ptr = new Printer(new BufferedWriter(
-									new OutputStreamWriter(System.out)));
-
-							ptr.flush();
-
-						} else {
-							System.out.println(v.value.toString());
-						}
-
+					if (v.value instanceof Node) {
+						Module result = (Module)v.value;
+						Environment env = new Environment(inputStream, System.out);
+						BuiltIns.inject(env);
+						env.runModule(result);
 					} else {
-						ParseError err = (ParseError) r;
-						if (-1 == err.index) {
-							System.err.println("  Parse error");
-						} else {
-							System.err.println("  " + p.location(err.index)
-									+ ": " + err.msg);
-						}
+						throw new InvalidInputException();
 					}
 
-				} catch (Throwable x) {
-					while (null != x.getCause()) {
-						x = x.getCause();
-					}
-					x.printStackTrace();
-				} finally {
-					try {
-						in.close();
-					} catch (Throwable x) {
-						/* Ignore. */
+				} else {
+					ParseError err = (ParseError) r;
+					if (-1 == err.index) {
+						System.err.println("  Parse error");
+					} else {
+						System.err.println("  " + p.location(err.index)
+								+ ": " + err.msg);
 					}
 				}
+
+			} catch (Throwable x) {
+				while (null != x.getCause()) {
+					x = x.getCause();
+				}
+				x.printStackTrace();
+			} finally {
+				try {
+					in.close();
+				} catch (Throwable x) {
+					/* Ignore. */
+				}
 			}
+
+			
 		}
 	}
 
